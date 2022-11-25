a8c345316d3b3f20103e0c5f88d237d036689cc9
Aryan <ac0453@srmist.edu.in>
2022-11-26 00:43:15 +0530

    repositary

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void Playsound(int sound_num){
+    AudioPlayer audioPlayer = AudioPlayer();
+    dynamic file = audioPlayer.audioCache.load("note$sound_num.wav");
+    audioPlayer.play(file);
+
+  }
+  Expanded Sevenkey({Color color , int sound_num}){
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed:(){
+          Playsound(sound_num);
+      }
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor:Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Sevenkey(color: Colors.red, sound_num: 1),
+              Sevenkey(color: Colors.orange, sound_num:2),
+              Sevenkey(color: Colors.yellowAccent, sound_num:3),
+              Sevenkey(color: Colors.green, sound_num:4),
+              Sevenkey(color: Colors.blue, sound_num:5),
+              Sevenkey(color: Colors.indigo, sound_num:6),
+              Sevenkey(color: Colors.purple, sound_num:7),
+            ],
+          )
         ),
       ),
     );
